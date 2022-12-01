8d42fa5507296afd7f9d070affc90e7cf0bc9fc1
1valerich1@mail.ru <1valerich1@mail.ru>
2022-12-01 11:15:12 +0400

    Added task19

---
+++ task19/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if((n/10000) == (n % 10) &&
+   ((n / 1000) % 10) == ((n / 10) % 10)) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
