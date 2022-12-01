507719984068987b44f5abf740d7ddd82b24f05b
1valerich1@mail.ru <1valerich1@mail.ru>
2022-12-01 11:31:00 +0400

    Added task21

---
+++ task21/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите координаты точки A: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+double y1 = Convert.ToInt32(Console.ReadLine());
+double z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты точки В: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+double y2 = Convert.ToInt32(Console.ReadLine());
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+Console.WriteLine(distance);
