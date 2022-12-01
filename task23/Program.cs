a1f929379ab71ae6c5c35694b99240c47508989a
1valerich1@mail.ru <1valerich1@mail.ru>
2022-12-01 11:43:40 +0400

    Added task23

---
+++ task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число = ");
+int n = Convert.ToInt32(Console.ReadLine());
+Double[] array = new Double[n];
+
+for(int i = 0; i < n; i++){
+    array[i] = Math.Pow(i+1,3);
+    Console.Write(array[i] + " ");
+}
